e8fbe581422d734860f6dc7b494bffb2e95778ba
tlaimer <tlaimer@localhost>
2011-04-12 14:56:07 +0200

    Reports Scaffolding kram mit ein paar Modifikationen (Dropdownliste anstatt direkte eingabe der ID)

--- IMS.WWW.ControlCenter/Controllers/ReportsController.cs
+++ IMS.WWW.ControlCenter/Controllers/ReportsController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using IMS.DataAccess;
 namespace IMS.WWW.ControlCenter.Controllers
 {
     public class ReportsController : Controller
@@ -11,16 +11,52 @@ namespace IMS.WWW.ControlCenter.Controllers
         //
         // GET: /Reports/
 
+		private IMSORMModelContainer _context = new IMSORMModelContainer();
+		public ReportsController()
+		{
+			if (!_context.DatabaseExists())
+				_context.CreateDatabase();
+		}
+
+		private Report GetReportById(Guid id)
+		{
+			return _context.Reports.First(r => r.ID == id);
+		}
+
+		private IEnumerable<SelectListItem> GetOperatorList()
+		{
+			return from op in _context.OnSiteOperators.ToList()
+				   select new SelectListItem {
+					   Text = op.Name,
+					   Value = op.ID.ToString(),
+					   Selected = false
+				   };
+		}
+
+		private IEnumerable<SelectListItem> GetPreselectedOperatorList(Guid operatorID)
+		{
+			return from op in _context.OnSiteOperators.ToList()
+				   select new SelectListItem {
+					   Text = op.Name,
+					   Value = op.ID.ToString(),
+					   Selected = op.ID == operatorID
+				   };
+		}
+
         public ActionResult Index()
         {
-            return View();
+			var reports = _context.Reports;
+			ViewData.Model = reports;
+            return View("List");
         }
 
         //
         // GET: /Reports/Details/5
 
-        public ActionResult Details(int id)
+        public ActionResult Details(Guid id)
         {
+			var report = GetReportById(id);
+			ViewData.Model = report;
             return View();
         }
 
@@ -29,6 +65,8 @@ namespace IMS.WWW.ControlCenter.Controllers
 
         public ActionResult Create()
         {
+			ViewBag.OperatorList = GetOperatorList();
+			ViewData.Model = new Report { CreateDate = DateTime.Now };
             return View();
         } 
 
@@ -36,12 +74,12 @@ namespace IMS.WWW.ControlCenter.Controllers
         // POST: /Reports/Create
 
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Report report)
         {
             try
             {
-                // TODO: Add insert logic here
-
+				_context.Reports.AddObject(report);
+				_context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -53,8 +91,11 @@ namespace IMS.WWW.ControlCenter.Controllers
         //
         // GET: /Reports/Edit/5
  
-        public ActionResult Edit(int id)
+        public ActionResult Edit(Guid id)
         {
+			var report = GetReportById(id);
+			ViewData.Model = report;
+			ViewBag.OperatorList = GetPreselectedOperatorList(report.OperatorID);
             return View();
         }
 
@@ -62,12 +103,15 @@ namespace IMS.WWW.ControlCenter.Controllers
         // POST: /Reports/Edit/5
 
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(Report report)
         {
             try
             {
-                // TODO: Add update logic here
- 
+				var old = GetReportById(report.ID);
+				old.OperatorID = report.OperatorID;
+				old.Text = report.Text;
+				old.CreateDate = report.CreateDate;
+				_context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -79,8 +123,10 @@ namespace IMS.WWW.ControlCenter.Controllers
         //
         // GET: /Reports/Delete/5
  
-        public ActionResult Delete(int id)
+        public ActionResult Delete(Guid id)
         {
+			var report = GetReportById(id);
+			ViewData.Model = report;
             return View();
         }
 
@@ -88,12 +134,12 @@ namespace IMS.WWW.ControlCenter.Controllers
         // POST: /Reports/Delete/5
 
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(Report report)
         {
             try
             {
-                // TODO: Add delete logic here
- 
+				_context.Reports.DeleteObject(GetReportById(report.ID));
+				_context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
