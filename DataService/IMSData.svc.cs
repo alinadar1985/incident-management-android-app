768cdcdae1abba11d4894ac1c7dcb35181adaef1
tlaimer <tlaimer@localhost>
2011-04-12 15:42:07 +0200

    DataService hinzu

---
+++ DataService/IMSData.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+using IMS.DataAccess;
+namespace DataService
+{
+    public class IMSData : DataService<IMSORMModelContainer>
+    {
+        // Diese Methode wird nur einmal aufgerufen, um dienstweite Richtlinien zu initialisieren.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+			config.SetEntitySetAccessRule("*", EntitySetRights.All);
+			config.SetServiceOperationAccessRule("*", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+    }
+}
