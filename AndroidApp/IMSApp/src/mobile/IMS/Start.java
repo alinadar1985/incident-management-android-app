3849ed9e7be9ac0bc7017b9099f7f2e2a12e3f5d
tlaimer <tlaimer@localhost>
2011-04-12 16:46:52 +0200

    app hinzu

---
+++ AndroidApp/IMSApp/src/mobile/IMS/Start.java
@@ -0,0 +1,16 @@
+package mobile.IMS;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+
+public class Start extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
