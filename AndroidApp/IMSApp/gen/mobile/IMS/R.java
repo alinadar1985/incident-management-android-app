f400df10075861d8b0938a890ab3483449661855
tlaimer <tlaimer@localhost>
2011-05-22 18:50:11 +0200

    verpeilt update und so

--- AndroidApp/IMSApp/gen/mobile/IMS/R.java
+++ AndroidApp/IMSApp/gen/mobile/IMS/R.java
@@ -8,23 +8,39 @@
 package mobile.IMS;
 
 public final class R {
+    public static final class array {
+        public static final int report_priorities=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int editTextIncidentDescription=0x7f050000;
-        public static final int reportSubmitButton=0x7f050001;
+        public static final int editTextIncidentDescription=0x7f060002;
+        public static final int new_waypoint_button=0x7f060005;
+        public static final int newincidentreportbutton=0x7f060004;
+        public static final int reportSubmitButton=0x7f060003;
+        public static final int report_btn_priority=0x7f060000;
+        public static final int report_edit_location=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int start=0x7f030001;
+        public static final int waypoint=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int opertor_guid=0x7f040004;
-        public static final int report_btn_submit=0x7f040003;
-        public static final int report_txt_label=0x7f040002;
+        public static final int app_name=0x7f050001;
+        public static final int ask_for_photo=0x7f050006;
+        public static final int hello=0x7f050000;
+        public static final int new_waypoint_button=0x7f05000b;
+        public static final int newincidentbutton=0x7f050005;
+        public static final int opertor_guid=0x7f050004;
+        public static final int report_btn_priority=0x7f050008;
+        public static final int report_btn_submit=0x7f050003;
+        public static final int report_lbl_description=0x7f050009;
+        public static final int report_lbl_location=0x7f05000a;
+        public static final int report_lbl_priority=0x7f050007;
+        public static final int report_txt_label=0x7f050002;
     }
 }
--- AndroidApp/IMSApp/src/mobile/IMS/Start.java
+++ AndroidApp/IMSApp/src/mobile/IMS/Start.java
@@ -1,49 +1,100 @@
 package mobile.IMS;
 
-import java.util.Date;
+import java.io.File;
 import java.util.UUID;
-
-import mobile.IMS.communication.*;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.*;
-import android.view.View.OnClickListener;
-import android.widget.*;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.DialogInterface;
+import android.content.Intent;
 
-public class Start extends Activity {
+public class Start extends Activity implements View.OnClickListener {
+	private static final int SCAN_BARCODE = 0xD00DE;
+	private static final int TAKE_IMAGE_REQUEST = 0xDEAD;
 
-	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		setupListeners();
+		setContentView(R.layout.start);
+		Button reportButton = (Button) findViewById(R.id.newincidentreportbutton);
+		reportButton.setOnClickListener(this);
+
+		((Button) findViewById(R.id.new_waypoint_button)).setOnClickListener(this);
+		Intent serviceIntent = new Intent(this, BackgroundService.class);
+		startService(serviceIntent);
 	}
 
-	private View.OnClickListener buttonClickEventHandler = new OnClickListener() {
-		public void onClick(View v) {
-			TextView textView = (TextView) findViewById(R.id.editTextIncidentDescription);
-			// textView.setText(R.string.hello);
-			RestClient client = new RestClient();
-			Report report = new Report(
-					new Date(),
-					UUID.randomUUID().toString(),
-					"27ac3e4e-a17e-419a-a2e6-68eaa55f1d4f",
-					textView.getText().toString(), 
-					null);
-			// OMG, hatte schon ganz vergessen was an Java so richtig scheiﬂe
-			// ist....
-			try {
-				client.addReport(report);
-			} catch (Exception e) {
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.newincidentreportbutton:
+			Intent reportIntent = new Intent(this, Report.class);
+			reportIntent.putExtra(Report.REPORT_UUID, UUID.randomUUID());
+			startActivity(reportIntent);
+		case R.id.new_waypoint_button:
+			AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+			dialogBuilder
+					.setPositiveButton("Scan barcode",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog, int which) {
+									Intent intent = new Intent(
+											"com.google.zxing.client.android.SCAN");
+									intent.setPackage("com.google.zxing.client.android");
+									startActivityForResult(intent, SCAN_BARCODE);
+								}
+							})
+					.setNeutralButton("Shoot Photo",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog, int which) {
+									String photoFileName = UUID.randomUUID().toString()
+											+ ".jpg";
+									File photoPath = new File(Environment
+											.getExternalStorageDirectory(),
+											"waypoints");
+									// ensure that /incidents directy exist
+									if (!photoPath.exists()) {
+										boolean success = photoPath.mkdirs();
+										if (!success) {
+											throw new RuntimeException(
+													"Could not create Image path!");
+										}
+									}
+									// save camera data in this file
+									File photoFile = new File(photoPath, photoFileName);
+									Uri photoUri = Uri.fromFile(photoFile);
+									Intent captureImage = new Intent(
+											MediaStore.ACTION_IMAGE_CAPTURE);
+									captureImage.putExtra(MediaStore.EXTRA_OUTPUT,
+											photoUri);
+									captureImage.putExtra(MediaStore.EXTRA_SIZE_LIMIT,
+											240 * 1024);
+									startActivityForResult(captureImage,
+											TAKE_IMAGE_REQUEST);
+								}
+							})
+					.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {}
+					})
+					.setCancelable(true)
+					.setTitle("Barcode or Photo?")
+					.show();
+		}
+	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case SCAN_BARCODE:
+			if (resultCode == RESULT_OK) {
+				String barcode = data.getStringExtra("SCAN_RESULT");
+				Toast.makeText(this, barcode, Toast.LENGTH_LONG).show();
 			}
 		}
-	};
 
-	private void setupListeners() {
-		Button button = (Button) findViewById(R.id.reportSubmitButton);
-		button.setOnClickListener(buttonClickEventHandler);
 	}
-
 }
