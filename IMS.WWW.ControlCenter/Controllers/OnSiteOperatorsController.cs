b9ab2a6cf99ba35ac52c0cf67dd3c1ade731b354
tlaimer <tlaimer@localhost>
2011-05-30 23:18:40 +0200

    neuer operator bekommt automatisch zuf?llige guid azure experimente raus

--- IMS.WWW.ControlCenter/Controllers/OnSiteOperatorsController.cs
+++ IMS.WWW.ControlCenter/Controllers/OnSiteOperatorsController.cs
@@ -60,35 +60,9 @@ namespace IMS.WWW.ControlCenter.Controllers
 		public ActionResult Create(OnSiteOperator osOperator)
 		{
 			try {
-				// TODO: Add insert logic here
+				osOperator.OperatorID = Guid.NewGuid();
 				_context.OnSiteOperators.AddObject(osOperator);
-				var oId = osOperator.OperatorID;
-				var account = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("DataConnectionString"));
-				var client = account.CreateCloudBlobClient();
-				//var cntnr = client.GetContainerReference("blub").GetPermissions().SharedAccessPolicies.Add(
-				var container = client.GetContainerReference(oId.ToString().ToLower());
-				container.CreateIfNotExist();
-				
-				var sas = container.GetSharedAccessSignature(new SharedAccessPolicy {
-					SharedAccessExpiryTime = DateTime.UtcNow.AddHours(24),
-					SharedAccessStartTime = DateTime.UtcNow,
-					Permissions = SharedAccessPermissions.Write | SharedAccessPermissions.Read, 
-				});
-				
-				var sap = new SharedAccessPolicy {
-					Permissions = SharedAccessPermissions.Write,
-					SharedAccessStartTime = DateTime.UtcNow,
-					SharedAccessExpiryTime = DateTime.UtcNow.AddYears(1).ToUniversalTime()
-				};
-
-				var bcp = new BlobContainerPermissions() {
-					PublicAccess = BlobContainerPublicAccessType.Off
-				};
-				
-
-
 				_context.SaveChanges();
-
 				return RedirectToAction("Index");
 			} catch {
 				return Content("Something went wrong");
