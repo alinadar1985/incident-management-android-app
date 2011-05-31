4c4102ee82a0e0d856245d63184c1c5e61efe635
tlaimer <tlaimer@localhost>
2011-05-31 13:51:06 +0200

    service wieder rein

---
+++ AndroidApp/IMSApp/src/mobile/IMS/BackgroundService.java
@@ -0,0 +1,26 @@
+package mobile.IMS;
+
+import java.util.Timer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BackgroundService extends Service {
+
+	private final Timer timer = new Timer(true);
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+
+		return null;
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		timer.schedule(new PhotoUploadTask(), 1000, 1000);
+		timer.schedule(new ReportUploadTask(), 5000, 2000);
+		return START_STICKY;
+	}
+}
