8947508c70971cb173fbcb8f684d41cf86db023e
tlaimer <tlaimer@localhost>
2011-05-31 13:53:32 +0200

    SaveReportData gibt jetzt eine Referenz auf das FileObjekt zur?ck

--- AndroidApp/IMSApp/src/mobile/IMS/ReportPersistence.java
+++ AndroidApp/IMSApp/src/mobile/IMS/ReportPersistence.java
@@ -2,16 +2,23 @@ package mobile.IMS;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Map;
 
 import mobile.IMS.util.FileByModifiedComparator;
 import mobile.IMS.util.FileBySuffixFilter;
 
 import org.simpleframework.xml.core.Persister;
+import org.simpleframework.xml.strategy.Strategy;
+import org.simpleframework.xml.strategy.Type;
+import org.simpleframework.xml.strategy.Value;
+import org.simpleframework.xml.stream.InputNode;
+import org.simpleframework.xml.stream.NodeMap;
+import org.simpleframework.xml.stream.OutputNode;
 
 import android.os.Environment;
 
 public class ReportPersistence {
-	public static void SaveReportData(ReportData data) throws Exception {
+	public static File SaveReportData(ReportData data) throws Exception {
 		Persister persister = new Persister();
 		File reportDir = new File(Environment.getExternalStorageDirectory(),
 				"report-data");
@@ -20,6 +27,7 @@ public class ReportPersistence {
 		File reportFile = new File(reportDir, data.getUuid().toString()
 				+ ".xml");
 		persister.write(data, reportFile);
+		return reportFile;
 	}
 
 	public static File TopReportDataFile() {
