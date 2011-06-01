c19ab9e1575f7a69efba71d638bb786e1aba46a5
tlaimer <tlaimer@localhost>
2011-06-01 12:31:36 +0200

    CSS getrennt, master/detail eingef?hrt, ajax load f?r detail

---
+++ IMS.WWW.ControlCenter/Scripts/report.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="jquery-1.4.4-vsdoc.js" />
+var addDetailLoad = function () {
+	$('a.details-link').each(function () {
+		var url = $(this).attr('href');
+		$(this).attr('href', '#'); // surpress reload
+		$(this).click(function () {
+			$('#detail-inner').html('');
+			alert(url);
+			$('#detail-inner').load(url);
+		});
+	});
+};
+
+$(document).ready(function () {
+	addDetailLoad();
+});
